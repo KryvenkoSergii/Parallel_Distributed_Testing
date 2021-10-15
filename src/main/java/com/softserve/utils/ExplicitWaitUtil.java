ae23bc6202577c78bac51e8a67a8a6bd0efaffe3
Sergii Kryvenko <krivenko.sergey@gmail.com>
2021-10-15 09:55:51 +0300

    improved tests stability N2

--- src/main/java/com/softserve/utils/ExplicitWaitUtil.java
+++ src/main/java/com/softserve/utils/ExplicitWaitUtil.java
@@ -21,17 +21,17 @@ import org.slf4j.LoggerFactory;
 import static org.awaitility.Awaitility.await;
 
 public class ExplicitWaitUtil {
-    
+
     private static final int PAGE_LOAD_TIMEOUT = 60;
     private static final int ELEMENT_PRESENT_TIMEOUT = 30;
     private static final int ELEMENT_CONDITION_TIMEOUT = 45;
-    private static final int POLLING_INTERVAL = 5;
+    private static final int POLLING_INTERVAL = 2;
     private static final int SCRIPT_TIMEOUT = 90;
-    private static final int CONDITION_MET_TIMEOUT = 120;
-    
+    private static final int CONDITION_MET_TIMEOUT = 30;
+
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
     private static ReadProjectProperties readProjectProperties = new ReadProjectProperties();
-    
+
     private WebDriver driver;
     private WebDriverWait wait;
     private FluentWait fluentWait;
@@ -41,10 +41,10 @@ public class ExplicitWaitUtil {
     public ExplicitWaitUtil(WebDriver driver) {
         this.driver = driver;
 //        this.wait = new WebDriverWait(driver, readProjectProperties.geExplicitWaitDelay());
-        this.wait = new WebDriverWait(driver, 30);
+        this.wait = new WebDriverWait(driver, 15);
     }
-    
- // Implicit wait - all elements
+
+    // Implicit wait - all elements
     public void setImplicitWait(int timeout) {
         driver.manage().timeouts().implicitlyWait(timeout, TimeUnit.SECONDS);
     }
@@ -62,10 +62,8 @@ public class ExplicitWaitUtil {
 
     // Fluent wait
     public void setFluentWait() {
-        fluentWait = wait
-                .withTimeout(Duration.of(ELEMENT_CONDITION_TIMEOUT, ChronoUnit.SECONDS))
-                .pollingEvery(Duration.of(POLLING_INTERVAL, ChronoUnit.SECONDS))
-                .ignoring(WebDriverException.class);
+        fluentWait = wait.withTimeout(Duration.of(ELEMENT_CONDITION_TIMEOUT, ChronoUnit.SECONDS))
+                .pollingEvery(Duration.of(POLLING_INTERVAL, ChronoUnit.SECONDS)).ignoring(WebDriverException.class);
     }
 
     // Explicit fluent wait - specific elements
@@ -86,90 +84,81 @@ public class ExplicitWaitUtil {
     }
 
     public void setAwait() {
-        await = await().atMost(CONDITION_MET_TIMEOUT, TimeUnit.SECONDS)
-                .pollInterval(POLLING_INTERVAL, TimeUnit.SECONDS);
+        await = await().atMost(CONDITION_MET_TIMEOUT, TimeUnit.SECONDS).pollInterval(POLLING_INTERVAL, TimeUnit.SECONDS);
     }
 
     public void awaitFor(Callable<Boolean> condition) {
         await.until(condition);
     }
-    
+
     public void elementToBeClickable(WebElement webElement) {
         logger.trace("wait until an element is clickable");
-        driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-        wait.until(ExpectedConditions
-                .elementToBeClickable(webElement));
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        setImplicitWait(0);
+        wait.until(ExpectedConditions.elementToBeClickable(webElement));
+        setImplicitWait(5);
     }
-    
+
     public void visibilityOfWebElement(WebElement webElement) {
         logger.trace("wait until a webElement is visible");
-        driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-        wait.until(ExpectedConditions
-                .visibilityOf(webElement));
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        setImplicitWait(0);
+        wait.until(ExpectedConditions.visibilityOf(webElement));
+        setImplicitWait(5);
     }
-    
+
     public void invisibilityOfWebElement(WebElement webElement) {
         logger.trace("wait until a webElement is visible");
-        driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-        wait.until(ExpectedConditions
-                .invisibilityOf(webElement));
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        setImplicitWait(0);
+        wait.until(ExpectedConditions.invisibilityOf(webElement));
+        setImplicitWait(5);
     }
-    
+
     public void presenceOfElementLocated(By locator) {
         logger.trace("wait until an element is present");
-        driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-        wait.until(ExpectedConditions
-                .presenceOfElementLocated(locator));
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        setImplicitWait(0);
+        wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+        setImplicitWait(5);
     }
-    
+
     public void absenceOfElementLocated(By locator) {
         logger.trace("wait until an element is absence");
-        driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-        wait.until(ExpectedConditions.not(ExpectedConditions
-                .presenceOfElementLocated(locator)));
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        setImplicitWait(0);
+        wait.until(ExpectedConditions.not(ExpectedConditions.presenceOfElementLocated(locator)));
+        setImplicitWait(5);
     }
-    
+
     public void elementToBeSelected(WebElement webElement) {
         logger.trace("wait until an element is visible");
-        driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-        wait.until(ExpectedConditions
-                .elementToBeSelected(webElement));
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        setImplicitWait(0);
+        wait.until(ExpectedConditions.elementToBeSelected(webElement));
+        setImplicitWait(5);
     }
-    
+
     public void elementHasStyleDisplayNone(String selectorXPath) {
         logger.trace("wait until an element has CSS Style");
-        driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-        wait.until(ExpectedConditions
-                .presenceOfElementLocated(By.xpath(selectorXPath + "[contains(@style, 'display: none')]")));
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        setImplicitWait(0);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(selectorXPath + "[contains(@style, 'display: none')]")));
+        setImplicitWait(5);
     }
-    
+
     public void elementIsStalenessOf(WebElement webElement) {
         logger.trace("wait until an element is no longer attached to the DOM");
-        driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-        wait.until(ExpectedConditions
-                .stalenessOf(webElement));
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        setImplicitWait(0);
+        wait.until(ExpectedConditions.stalenessOf(webElement));
+        setImplicitWait(5);
     }
-    
+
     public void waitForPageLoadComplete(long timeToWait) {
-        new WebDriverWait(driver, timeToWait).until(
-            webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
+        new WebDriverWait(driver, timeToWait)
+                .until(webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
     }
 
     public void waitForAjaxToComplete(long timeToWait) {
-        new WebDriverWait(driver, timeToWait).until(
-            webDriver -> ((JavascriptExecutor) webDriver).executeScript("return window.jQuery != undefined && jQuery.active == 0;"));
+        new WebDriverWait(driver, timeToWait).until(webDriver -> ((JavascriptExecutor) webDriver)
+                .executeScript("return window.jQuery != undefined && jQuery.active == 0;"));
     }
-    
+
     public void waitForAjaxToCompletePdp(long timeToWait) {
-        new WebDriverWait(driver, timeToWait).until(
-            webDriver -> ((JavascriptExecutor) webDriver).executeScript("return window.jQuery != undefined && jQuery.active <=2;"));
+        new WebDriverWait(driver, timeToWait).until(webDriver -> ((JavascriptExecutor) webDriver)
+                .executeScript("return window.jQuery != undefined && jQuery.active <=2;"));
     }
 }
